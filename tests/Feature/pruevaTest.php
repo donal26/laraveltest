97fa4fb073268cf378efa6eb5ff254010fe5462f
donal26 <donal.cantaro@unas.edu.pe>
2018-07-18 21:32:09 -0500

    subiendo py laravel

---
+++ tests/Feature/pruevaTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class pruevaTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     */
+     public function testConsumer($a, $b)
+     {
+         $this->assertSame('first', $a);
+         $this->assertSame('second', $b);
+     }
+}
